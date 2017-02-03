4d016fd78db216ca0d75fab8a6a81e60ea59ecd8
hgarciaalberto <hgarcia.alberto@gmail.com>
2017-02-03 15:44:41 +0100

    * ADD: Subida de la creación del layout con el sidenav

---
+++ AhgItDevelopmentWebSite/Core/app.js
@@ -0,0 +1,29 @@
+﻿"use strict";
+
+angular.module("website", [
+    "ngRoute",
+    "ngAnimate",
+    "ngMaterial",
+    "website.urls"])
+    .config([
+        "$routeProvider",
+        "$locationProvider",
+        "$mdThemingProvider",
+        "urlsProvider",
+        function ($routeProvider, $locationProvider, $mdThemingProvider, urlsProvider) {
+            $routeProvider
+                .when("/", {
+                    redirectTo: "/" + urlsProvider.$get().login
+                })
+                .otherwise({
+                    redirectTo: "/"
+                });
+
+            $locationProvider.html5Mode(true);
+
+            $mdThemingProvider.theme("default")
+                .primaryPalette("indigo")
+                .accentPalette("red")
+                .warnPalette("deep-orange")
+                .backgroundPalette("grey");
+        }]);
---
+++ AhgItDevelopmentWebSite/Core/urls.factory.js
@@ -0,0 +1,34 @@
+﻿angular.module("website.urls", [])
+    .config([
+        "$locationProvider",
+        function ($locationProvider) {
+            $locationProvider.html5Mode(true);
+        }])
+    .factory("urls", [
+        "$location",
+        function ($location) {
+            var urls = {};
+            urls.empresa = "empresa";
+            urls.contacto = "contacto";
+            urls.apps = "apps"
+            urls.armeria = "armeria";
+            urls.armeria.politicaPrivacidad = "politicaprivacidad";
+            urls.horario = "horario";
+            urls.horario.politicaPrivacidad = "politicaprivacidad";
+
+            //FIX: encodeURI si tiene espacios: http://curran.github.io/screencasts/introToAngular/exampleViewer/#/44
+            urls.goToPage = function (url) {
+                $location.url(url);
+                urls.currentPage = url;
+            };
+
+            urls.goToFirstPage = function () {
+                urls.goToPage(urls.empresa);
+            };
+
+            urls.isCurrentPage = function (url) {
+                return url == urls.currentPage;
+            };
+
+            return urls;
+        }]);
